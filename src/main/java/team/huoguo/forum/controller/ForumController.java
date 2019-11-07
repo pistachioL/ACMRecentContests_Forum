2b69c04b613879b0b67915ddc2e3914dc272fd3b
pistachiol <785409363@qq.com>
2019-11-07 17:33:57 +0800

    显示数据接口

--- src/main/java/team/huoguo/forum/controller/ForumController.java
+++ src/main/java/team/huoguo/forum/controller/ForumController.java
@@ -5,22 +5,25 @@ import org.springframework.web.bind.annotation.*;
 import team.huoguo.forum.bean.Forum;
 import team.huoguo.forum.repository.ForumRepository;
 
-import java.util.Date;
+import java.util.List;
 
 @RestController
 @ResponseBody
+
  public class ForumController {
     @Autowired
     ForumRepository forumRepository;  //注入实例
+    @RequestMapping("postComment")
 
-    @PostMapping("/post")
-    public Object postForum(@RequestParam(value="post_title ",required = false,defaultValue = "南京网络赛")String title,
-                            @RequestParam(value="post_content ",required = false,defaultValue = "网路赛万千,你们有没有觉得这次的网络赛难点很多,而且在字符串上有坑")String content,
-                            @RequestParam(value="post_date ",required = false,defaultValue = "2019-10-24")Date date){
-
-                    Forum forum = new Forum(title,content,date);
-                    return forumRepository.save(forum);
-
+    public Forum postForum(@RequestBody Forum forum){   //发送评论内容
+            return forumRepository.save(forum);
 
     }
+
+    @GetMapping(value = "getTitle")
+    public List<Forum> getTitle(){   //获取评论标题
+        return forumRepository.findAll();
+    }
+
+
 }
--- src/test/java/team/huoguo/forum/AcmRecentContestsForumApplicationTests.java
+++ src/test/java/team/huoguo/forum/AcmRecentContestsForumApplicationTests.java
@@ -5,16 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import team.huoguo.forum.controller.ForumController;
 
-import java.util.Date;
-
 @SpringBootTest
 class AcmRecentContestsForumApplicationTests {
 
 	@Autowired
 	ForumController forumController;
+
 	@Test
 	void contextLoads() {
-		forumController.postForum("123","123",new Date(System.currentTimeMillis()));
+
 	}
 
 }
